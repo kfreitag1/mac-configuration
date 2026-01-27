c02bd2abb4363f3921e6572249b306b6e0836386
Kieran Freitag <kfreitag@kieran.ca>
2026-01-27 06:56:07 -0800

    work brews and config

--- nix/modules/work.nix
+++ nix/modules/work.nix
@@ -1,16 +1,24 @@
 { pkgs, ... }:
 {
+  # Ensure Determinate manages nix, not nix-darwin
+  nix.enable = false;
+
   environment.systemPackages = with pkgs; [
 
   ];
 
   homebrew = {
     enable = true;
-    casks = [
-
-    ];
+    taps = [ ];
     brews = [
-
+      "yarn"
+      "promptfoo"
+      "ykman"
+      "docker"
+      "colima"
+      "docker-compose"
+      "ruby"
+      "pnpm"
     ];
   };
 }
